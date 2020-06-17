6fbe4ef6b1cc2dff28e2d5feb0c8084fa21547b3
Shoma FUKUDA <fkshom@gmail.com>
2020-06-17 23:30:13 +0900

    add accessor method to raw element

--- src/pyte_prism/__init__.py
+++ src/pyte_prism/__init__.py
@@ -97,6 +97,18 @@ class SupportMethodGenerator(object):
         return True
     return inner
 
+  def element_element(self, by, selector):
+    this = self
+    def inner(self):
+      return self.driver.find_element(by, selector)
+    return inner
+
+  def element_elements(self, by, selector):
+    this = self
+    def inner(self):
+      return self.driver.find_elements(by, selector)
+    return inner
+
 class Section(object):
   def __init__(self, klass, base_by, base_selector):
     self.klass = klass
@@ -138,6 +150,12 @@ class PageMetaclass(type):
         # Elementsのときもfind_elementが使われるため、「少なくとも1つのelementがあるかどうか」が検査される
         dict_[f"has_{k}"] = smg.has_element(v.by, v.selector)
         dict_[f"has_no_{k}"] = smg.has_no_element(v.by, v.selector)
+
+        if isinstance(v, Element):
+          dict_[f"{k}_element"] = smg.element_element(v.by, v.selector)
+        elif isinstance(v, Elements):
+          dict_[f"{k}_elements"] = smg.element_elements(v.by, v.selector)
+
       if isinstance(v, Section) or isinstance(v, Sections) or isinstance(v, Iframe):
         smg = SupportMethodGenerator()
         dict_[f"wait_until_{k}_visible"] = smg.wait_until_element_visible(v.base_by, v.base_selector)
@@ -145,6 +163,14 @@ class PageMetaclass(type):
         # Sectionsのときもfind_elementが使われるため、「少なくとも1つのelementがあるかどうか」が検査される
         dict_[f"has_{k}"] = smg.has_element(v.base_by, v.base_selector)
         dict_[f"has_no_{k}"] = smg.has_no_element(v.base_by, v.base_selector)
+
+        if isinstance(v, Section):
+          dict_[f"{k}_element"] = smg.element_element(v.by, v.selector)
+        elif isinstance(v, Sections):
+          dict_[f"{k}_elements"] = smg.element_elements(v.by, v.selector)
+        elif isinstance(v, Iframe):
+          dict_[f"{k}_element"] = smg.element_element(v.by, v.selector)
+
 
     return type.__new__(cls, name, bases, dict_)
 
